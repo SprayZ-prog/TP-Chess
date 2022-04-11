622f53317d13f6623f57315cd4711ed5e941d570
SprayZ-prog <lfveillette@outlook.com>
2022-04-11 18:12:20 -0400

    pieceMemoire et polymorphisme

--- Echecs/Modeles/Piece.cs
+++ Echecs/Modeles/Piece.cs
@@ -10,14 +10,10 @@ namespace Echecs
     {
         protected char _nom;
         protected Couleur _couleur;
-        protected bool _aBouge;
-        protected bool _peutEtrePromu;
 
         public Piece(Couleur couleur)
         {
-            _aBouge = false;
             _couleur = couleur;
-            _peutEtrePromu = false;
         }
 
         public Couleur Couleur
@@ -26,24 +22,15 @@ namespace Echecs
             set { _couleur = Couleur; }
             
         }
-        public bool PeutEtrePromu
+        public virtual bool peutEtrePromu()
         {
-            get { return _peutEtrePromu; }
-        }
-        public bool Abouge
-        {
-            get { return _aBouge; }
-            set { _aBouge = value; }
+            return false;
         }
         public virtual Mouvement regles(int indexInitiale, int indexDestination)
         {
             return Mouvement.peutPasBouger;
         }
         
-        public virtual bool estEssentiel()
-        {
-            return false;
-        }
 
 
     }
---
+++ Echecs/PieceMemoire.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echecs
+{
+    public class PieceMemoire : Piece
+    {
+        public PieceMemoire(Couleur couleur) : base(couleur)
+        {
+            
+        }
+        public virtual bool aBougé()
+        {
+            return false;
+        }
+
+    }
+}
